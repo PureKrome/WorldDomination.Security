774960ebd4aae70a6fbf3d14204eff8d1c67e400
Pure Krome <pure.krome@world-domination.com.au>
2013-01-05 23:08:51 +1100

    :boom: Final changes for initial NuGet release.
    - Removed the Mvc project.
    - Added some simple ClaimsPrincipalExtensions.
    - Created NuGet package 0.1.

---
+++ Code/WorldDomination.Security/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+
+namespace WorldDomination.Security
+{
+    public static class ClaimsExtensions
+    {
+        public static string WithUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.HasClaim(x => x.Type == ClaimTypes.Uri)
+                       ? claimsPrincipal.FindFirst(x => x.Type == ClaimTypes.Uri).Value
+                       : null;
+        }
+
+        public static string WithDisplayName(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.HasClaim(x => x.Type == ClaimTypes.Name)
+                       ? claimsPrincipal.FindFirst(x => x.Type == ClaimTypes.Name).Value
+                       : null;
+        }
+
+        public static string WithPictureUri(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.HasClaim(x => x.Type == CustomClaimsTypes.PictureUri)
+                       ? claimsPrincipal.FindFirst(x => x.Type == CustomClaimsTypes.PictureUri).Value
+                       : null;
+        }
+    }
+}
